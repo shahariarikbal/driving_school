d4e0f6693703bb66126a579ba96556fb315dcb27
shahariarikbal <shahariar.ikbal86@gmail.com>
2022-05-25 23:55:07 +0600

    feat:Model and Migration done

---
+++ database/migrations/2022_05_25_165212_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('category_id');
+            $table->text('question');
+            $table->string('image');
+            $table->boolean('answer')->default();
+            $table->text('explanation');
+            $table->unsignedInteger('marks')->default(1);
+            $table->boolean('is_active')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
